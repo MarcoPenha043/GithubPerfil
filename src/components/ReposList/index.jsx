5f96d8271c75a0c6159f2bb6a560187803b3fee1
Marco Aurélio Penha <marcopenha.dev@gmail.com>
2024-12-20 19:23:28 -0300

    terceiro commit

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import styles from './ReposList.module.css';
+import styles from './Reposlist.module.css';
 
 const ReposList = ({ nomeUsuario }) => {
     const [repos, setRepos] = useState([]);
